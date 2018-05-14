7d0147ed8286f7259d33e03588d869a8489c6f7a
Shernandezglez <32920669+Shernandezglez@users.noreply.github.com>
2018-05-13 21:21:41 -0500

    Add files via upload

---
+++ ListasDoblementeEnlazadas/ListasDoblementeEnlazadas/ClaseBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDoblementeEnlazadas
+{
+    class ClaseBase
+    {
+        private int _dato;
+        private ClaseBase _siguiente;
+        private ClaseBase _anterior;
+
+
+        public ClaseBase Siguiente
+        {
+            get { return _siguiente;}
+            set { _siguiente = value; }
+        }
+
+        public ClaseBase Anterior
+        {
+            get { return _anterior; }
+            set { _anterior = value; }
+        }
+
+        public int Dato
+        {
+            get { return _dato; }
+            set { _dato = value; }
+        }
+
+        public ClaseBase(int dato)
+        {
+            _dato = dato;
+        }
+
+        public override string ToString()
+        {
+            return "Dato :" + _dato;
+        }
+    }
+}
---
+++ ListasDoblementeEnlazadas/ListasDoblementeEnlazadas/Estructura.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDoblementeEnlazadas
+{
+    class Estructura
+    {
+        ClaseBase inicio = null;
+
+        public void agregarObjeto(ClaseBase nuevo)
+        {
+            if (inicio == null)
+                inicio = nuevo;
+            else
+            {
+
+            }
+        }
+
+    }
+}
